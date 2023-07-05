2c5320a0dd06580d2dcbd6d67e500805deaa386c
longtang <long.tang628@gmail.com>
2023-07-05 11:38:24 -0400

    Initial commit

---
+++ src/Serialization/Driver.java
@@ -0,0 +1,37 @@
+
+
+import static Serialization.Jackets.deserialize;
+import static Serialization.Jackets.serializeJacketCSV;
+
+/**
+ *
+ * Jacket Object Serialization and Deserialization:
+ * <p>
+ *     Printing out the original and deserialized jacket information, and checks for equality between the
+ *     original and deserialized jackets using the equals method
+ * </p>
+ *
+ * Note:
+ * <p>
+ *     Serialization store the state of an object
+ *     Imagine you creatd  a project and save the data to the file = savepoint
+ *
+ * </p>
+ *
+ */
+public class Driver {
+    public static void main(String[] args){
+        Jackets myJacket = new Jackets("North Face", "Black" , 299);
+        String filename = "jackets.csv";
+
+        serializeJacketCSV(myJacket, filename);
+        Jackets deserializedJacket = deserialize(filename);
+
+        deserializedJacket.checkEquality(myJacket);
+
+    }
+}
+
+
+
+
